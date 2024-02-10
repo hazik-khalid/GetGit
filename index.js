bcb6cfded42d3f8ee3df9b2405d13a993c9db39d
hazik-khalid <hazikkhalid111@google.com>
2024-02-10 14:53:22 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,47 +2,39 @@ const { App } = require('@slack/bolt');
 require('dotenv').config();
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
 });
 
 app.command('/gitget', async ({ command, ack, say }) => {
-    var userData; // global variable
-    var uname;
-    var name;
-    var avatar;
-    var repos;
-    var link;
-    var email;
-    await ack();
-  
-    const username = command.text;
-  
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      if (response.ok) {
-        userData = await response.json();
-        uname = userData.login;
-        name = userData.name;
-        avatar = userData.avatar_url;
-        repos = userData.repos_url;
-        link = userData.html_url;
-        email = userData.email;
-  
-        // Move the say() inside the try block
-        await say(`username:-"${uname}" name:-${name}, profile-link:-${link} email:- ${email}`);
-      } else {
-        console.error('Failed to fetch user data. Status:', response.status);
-      }
-    } catch (error) {
-      console.log(error);
+  var userData; // global variable
+  var uname;
+  var link;
+
+  await ack();
+
+  const username = command.text;
+
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    if (response.ok) {
+      userData = await response.json();
+      uname = userData.login;
+      link = userData.html_url;
+
+      // Move the say() inside the try block
+      await say(`UserName:- ${uname}  ${link} `);
+    } else {
+      console.error('Failed to fetch user data. Status:', response.status);
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 (async () => {
-    // Start your app
-    await app.start(process.env.PORT || 3000);
-    console.log(`⚡️ Bot app is running on port ${process.env.PORT || 3000}!`);
+  await app.start(process.env.PORT || 3000);
+  console.log(` Bot is running on port ${process.env.PORT || 3000}!`);
 })();
