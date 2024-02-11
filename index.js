f5ec5a5a1ab7cf0252419e15a435943370f3ca1c
hazik-khalid <hazikkhalid111@google.com>
2024-02-11 14:26:13 +0530

    token in index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const { App } = require('@slack/bolt');
 require('dotenv').config();
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: 'xoxb-6612383026307-6613327996995-xF1pahXQQd1vxCnCsfMBsW7b',
+  signingSecret: '4792af9566aa1f2024489a9219deaf25',
   socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
+  appToken: "xapp-1-A06HYCX5AUV-6607062783206-2d858b83952cd38ab8cbc273cd0d01d37ad826f4c5a7de615e0855c959019322",
 });
 
 app.command('/gitget', async ({ command, ack, say }) => {
-  var userData; 
+  var userData;
   var uname;
   var link;
 
